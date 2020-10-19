99ab3f282c8d199a3df1512ad2a95d858bb26210
sokoki86 <69558954+sokoki86@users.noreply.github.com>
2020-10-19 12:30:32 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function handleThumbnailClicks() {
+    $('.thumbnail').on('click', function(event){
+        const imgSrc = $(event.currentTarget).find('img').attr('src');
+        const imgAlt = $(event.currentTarget).find('img').attr('alt');
+
+        $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
+    })
+}
+  
+$(handleThumbnailClicks);
